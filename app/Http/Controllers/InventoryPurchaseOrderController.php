4bf7e8e26651ba72a4d03b1170db9a465342799b
Jerom Iruthayaraj <jeromraj99@gmail.com>
2022-04-17 20:19:13 +0530

    edit purchase controller

--- app/Http/Controllers/InventoryPurchaseOrderController.php
+++ app/Http/Controllers/InventoryPurchaseOrderController.php
@@ -105,5 +105,89 @@ class InventoryPurchaseOrderController extends Controller
         return redirect('/purchase-show-all')->with('success', 'Successfully Recorded');
     }
 
-}
+    public function purchaseEdit($id)
+    {
+        $sellers = InventorySeller::all();
+        $products = InventoryItem::all();
+        $purchase_order = InventoryPurchaseOrder::find($id);
+        $purchase_items = DB::table('inventory_purchase_items')
+            ->where('purchase_order_id', '=', $id)
+            ->get();
+           // return $purchase_order;
+        return view('inventory.purchase.purchaseEdit', compact('sellers', 'products', 'purchase_order', 'purchase_items'));
+    }
 
+    public function purchaseUpdateProcess(Request $request)
+    {
+        // return $request;
+        $purchase_order = InventoryPurchaseOrder::find($request->id);
+        $request->validate([
+            'bill_no' => 'nullable|unique:inventory_purchase_orders,pur_ord_bill_no,' . $purchase_order->id,
+            'cheque_no' => 'nullable|unique:inventory_purchase_orders,pur_ord_cheque_no,' . $purchase_order->id,
+            'amount' => 'required',
+            'seller_id' => 'required',
+            'product_id' => 'required',
+            'qty' => 'required',
+            'qty_type' => 'required',
+            'price' => 'required',
+            'image_1' => 'nullable|max:2048|mimes:png,gif,jpeg,jpg,pdf,doc',
+            'image_2' => 'nullable|max:2048|mimes:png,gif,jpeg,jpg,pdf,doc',
+            'image_3' => 'nullable|max:2048|mimes:png,gif,jpeg,jpg,pdf,doc'
+        ]);
+        $purchase_order = InventoryPurchaseOrder::find($request->id);
+        $purchase_order->pur_ord_bill_no = $request->bill_no;
+        $purchase_order->pur_ord_amount = $request->amount;
+        $purchase_order->pur_ord_cash = $request->cash_amount;
+        $purchase_order->pur_ord_cheque = $request->cheque_amount;
+        $purchase_order->pur_ord_cheque_no = $request->cheque_no;
+        $purchase_order->pur_ord_cheque_date = $request->cheque_date;
+        $purchase_order->pur_ord_online_or_card = $request->online_amount;
+        $purchase_order->pur_ord_reference_no = $request->reference_no;
+        $purchase_order->pur_ord_credit = $request->credit_amount;
+        $purchase_order->seller_id = $request->seller_id;
+
+        if ($request->img_1 != null) {
+            $image_name = $request->id . '-0.' . $request->img_1->extension();
+            $request->img_1->move('inventory_purchase', $image_name);
+            $purchase_order->bill_img_1 = $image_name;
+        }
+
+        if ($request->img_2 != null) {
+            $image_name = $request->id . '-1.' . $request->img_2->extension();
+            $request->img_2->move('inventory_purchase', $image_name);
+            $purchase_order->bill_img_2 = $image_name;
+        }
+
+        if ($request->img_3 != null) {
+            $image_name = $request->id . '-2.' . $request->img_3->extension();
+            $request->img_3->move('inventory_purchase', $image_name);
+            $purchase_order->bill_img_3 = $image_name;
+        }
+
+        $purchase_order->save();
+
+        $actvity = 'Update PurchaseOrder, Purchase_order_id :-'. $purchase_order->id;
+        $a = app('App\Http\Controllers\ActivityLogController')->index($actvity);
+
+        $permanent_purchase_item = InventoryPurchaseItem::where('purchase_order_id', '=', $purchase_order->id)->delete();
+        
+        $length = count($request->qty);
+        for ($i = 0; $i < $length; $i++) {
+                $permanent_purchase_item = new InventoryPurchaseItem();
+                $permanent_purchase_item->item_id = $request->product_id[$i];
+                $permanent_purchase_item->pur_item_qty = $request->qty[$i];
+                $permanent_purchase_item->pur_item_qty_type = $request->qty_type[$i];
+                $permanent_purchase_item->pur_item_amount = $request->price[$i];
+                $permanent_purchase_item->purchase_order_id = $purchase_order->id;
+                $permanent_purchase_item->warranty = $request->warrenty[$i];
+                $permanent_purchase_item->serial_number = $request->serial_number[$i];
+                $permanent_purchase_item->description = $request->description[$i];
+                $permanent_purchase_item->save();
+                
+                $actvity = 'Add New InventoryPurchaseItem, Inventory_purchase_item_id :- '. $permanent_purchase_item->id;
+                $a = app('App\Http\Controllers\ActivityLogController')->index($actvity);
+        }
+        return redirect('/purchase-show-all')->with('success', 'Successfully Updated');
+    }
+
+}
